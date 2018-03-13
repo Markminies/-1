7aa9e1e5ae86141680dc83bd26efe8af54144bb4
Markminies <Markminies@mail.ru>
2018-03-13 09:29:50 +0300

    Fery

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika1
+{
+    class kvit
+    {
+        public int nomer { get; set; }
+
+        private String data;
+        public int data1 { get; set; }
+
+        public double Summa { get; set; }
+
+        public kvit()
+        {
+            this.nomer = 0;
+            this.data1 = 0;
+            this.Summa = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введите номер квитанции: "); this.nomer = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату квитанции: "); this.data1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сумму квитанции: "); this.Summa = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Номер: { this.nomer }");
+            Console.WriteLine($"Дата: { this.data }");
+            Console.WriteLine($"Сумма : { this.Summa }");
+        }
+
+        static void Main(string[] args)
+        {
+            kvit scr = new kvit();
+            scr.Input();
+            scr.Print();
+            Console.ReadKey();
+        }
+
+    }
+}
